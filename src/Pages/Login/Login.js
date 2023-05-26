e28f8222c0c3866dfdbc3da0ec264dbb7d4070b0
mastewal12 <mastewaltakele8@gmail.com>
2023-05-26 02:12:20 -0500

    update

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -94,23 +94,19 @@ const Login = () => {
           <p className="forTitle">About</p>
           <h1>Evangadi Networks Q&A</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem
-            voluptate officiis beatae nobis pariatur omnis facere accusamus
-            laboriosam hic, adipisci vero reiciendis, recusandae sit ad, eum
-            quisquam! Molestias, ut commodi!
+            No matter what stage of life you are in, whether you’re just
+            starting elementary school or being promoted to CEO of a Fortune 500
+            company, you have much to offer to those who are trying to follow in
+            your footsteps.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem
-            voluptate officiis beatae nobis pariatur omnis facere accusamus
-            laboriosam hic, adipisci vero reiciendis, recusandae sit ad, eum
-            quisquam! Molestias, ut commodi!
+            Wheather you are willing to share your knowledge or you are just
+            looking to meet mentors of your own, please start by joining the
+            network here.
           </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-            ipsum, provident minus laudantium esse soluta maiores nostrum nisi
-            sunt perferendis dolorum. Praesentium necessitatibus quia
-            consectetur sunt tempora possimus eveniet voluptates?
-          </p>
+
+
+
           <button className="btn1">HOW IT WORKS</button>
         </div>
       </div>
